4108d349cf523946cd639bb25830e67735cec40a
Shashank Barot <164487273+ShashankBarot@users.noreply.github.com>
2025-08-09 16:45:44 +0000

    Resolved 502 Bad Gateway

--- main.py
+++ main.py
@@ -108,4 +108,5 @@ def health_check_head():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
